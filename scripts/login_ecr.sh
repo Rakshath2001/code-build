d84c283bb1f2ac76ad4bbe506f66fee063f8b9e0
rakshath <rakshathbr2001@gmail.com>
2026-02-16 17:45:03 +0530

    dummy commit 2

--- scripts/login_ecr.sh
+++ scripts/login_ecr.sh
@@ -4,7 +4,7 @@ REGION=ap-south-1
 ACCOUNT_ID=123456789012
 REPO_NAME=my-app-repo
 
-echo "Logging in to ECR..."
+echo "Logging in to ECR...."
 
 aws ecr get-login-password --region $REGION \
 | docker login --username AWS \
